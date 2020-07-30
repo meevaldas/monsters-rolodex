6874483aaa49db6eada28fa56076cf7193722934
meevaldas <meevadas13@gmail.com>
2020-07-31 00:22:24 +0530

    Updates

---
+++ precache-manifest.6c6e41924dd1d857e6a370842da6139f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91731a195ce7d5990b02aaaa2b262f62",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2538d5331dd1d15284c6",
+    "url": "/monsters-rolodex/static/css/main.c5c856c9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2538d5331dd1d15284c6",
+    "url": "/monsters-rolodex/static/js/main.fcfbc3b1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
